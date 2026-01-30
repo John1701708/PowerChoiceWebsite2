215e15edf0a39359652cbe2418ced113993fce9e
John1701708 <markgreen7383@gmail.com>
2026-01-30 10:04:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PlansPage from './pages/PlansPage';
-import EnrollmentPage from './pages/EnrollmentPage';
-import AboutPage from './pages/AboutPage';
-import HowItWorksPage from './pages/HowItWorksPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import BillingPage from './pages/BillingPage';
+import BlogPage from './pages/BlogPage';
+import CareersPage from './pages/CareersPage';
+import ContactPage from './pages/ContactPage';
+import EnrollmentPage from './pages/EnrollmentPage';
+import FAQPage from './pages/FAQPage';
+import HowitWorksPage from './pages/HowitWorksPage';
 import NotFoundPage from './pages/NotFoundPage';
+import PlansPage from './pages/PlansPage';
+import PrivacyPage from './pages/PrivacyPage';
+import SupportPage from './pages/SupportPage';
+import TermsPage from './pages/TermsPage';
+import TestimonialsPage from './pages/TestimonialsPage';
+import ZipCheckPage from './pages/ZipCheckPage';
+import FeaturesPage from './pages/FeaturesPage'; // last missing page
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/plans" element={<PlansPage />} />
-        <Route path="/enroll" element={<EnrollmentPage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/billing" element={<BillingPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/careers" element={<CareersPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/enrollment" element={<EnrollmentPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/how-it-works" element={<HowitWorksPage />} />
+        <Route path="/plans" element={<PlansPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="/terms" element={<TermsPage />} />
+        <Route path="/testimonials" element={<TestimonialsPage />} />
+        <Route path="/zip-check" element={<ZipCheckPage />} />
+        <Route path="/features" element={<FeaturesPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
